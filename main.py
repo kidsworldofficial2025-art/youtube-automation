e0cb81cf9e6ffaa73bb908cff484e34ccf118c7c
kidsworldofficial2025-art <kidsworldofficial2025@gmail.com>
2026-01-02 20:33:45 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Automation started...")
+
+# Example script (you can improve later)
+text = "Success is built one step at a time."
+
+with open("script.txt", "w") as f:
+    f.write(text)
+
+print("Script created successfully!")
